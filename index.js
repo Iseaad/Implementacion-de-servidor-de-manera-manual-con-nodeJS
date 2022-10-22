bc351128a9efc562dfdf96909e454c13106cd48e
iseaad <alejandro.giraldo.villada@gmail.com>
2022-10-22 17:02:15 -0500

    metodo get y post creados

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
-const express = require ("express");  //se importa modulo express y se guarda en la variable 
-const mongoose = require ("mongoose"); //importacion modulo mongoose
-const deporte  = require ('./modelos/deporte');
+const express=require ("express");  //se importa modulo express y se guarda en la variable 
+const mongoose=require ("mongoose"); //importacion modulo mongoose
+const Deporte = require("./modelos/deporte");
+const deporte=require ('./modelos/deporte'); //importacion de modelo deporte
 
 const app = new express (); //instanciando o inicializando objeto tipo express  
+const router=express.Router();
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
 
 //----------------conexiòn a la base de datos en mongodb atlas-------------------------//
 mongoose.connect("mongodb+srv://Grupo23:Grupo23@clustergrupo23.gvrtivl.mongodb.net/Grupo23?retryWrites=true&w=majority");
 
+//---------------CRUD HACIA LA BD-------------------//
+router.get('/',(req,res)=>{
+    console.log("Accediendo a la raiz")
+    res.send("Bienvenido a mi primera API")
+});
 
-//METODO listen nos pide dos parametros: 1. puerto al cual se conectará. 2. el objeto de tipo funcion call back.
-//se puede usar una función tipo flecha:
+router.post ('/creandoReporte', (req,res)=>{
+    let nuevoDeporte = new Deporte({
+        nombre: req.body.nombre,
+        numeroJugadores: req.body.jugadores,
+        pais: req.body.pais
+    });
+});
 
+app.use(router);
 app.listen (3000,()=>{  // si la aplicacion se conecta por el puerto 3000 ejecutará la function solicitada
 console.log ("Servidor ejecutandose por el puerto 3000")
 });
